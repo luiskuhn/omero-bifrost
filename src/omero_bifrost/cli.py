eb39e48a244a35a1fba9e2a0e0214b061d756619
Luis Kuhn Cuellar <38211686+luiskuhn@users.noreply.github.com>
2026-05-07 15:02:33 +0200

    Lazy-load FAIR schema import to unblock CLI help smoke test

--- src/omero_bifrost/cli.py
+++ src/omero_bifrost/cli.py
@@ -25,7 +25,6 @@ from rich import print
 from typing import Annotated, List
 
 from omero_bifrost.utils.filter_expr import FilterParseError, parse_filter_exprs
-from omero_bifrost.fair.metadata_schema import validate_row
 
 #####################################
 
@@ -249,6 +248,8 @@ def push_key_value(
     except FilterParseError as exc:
         conn.close()
         _handle_cli_error(exc)
+
+    from omero_bifrost.fair.metadata_schema import validate_row
 
     key_value_data = []
     warnings = []
